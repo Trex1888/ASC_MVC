25c08a6a9a884d230a8c0c2cdc2b08fe2b73d5af
Harpua <harpua123@gmail.com>
2023-08-06 17:42:07 -0500

    Update Edit and Delete functions to use GUID

--- WebApplication13/Controllers/HomeController.cs
+++ WebApplication13/Controllers/HomeController.cs
@@ -6,8 +6,7 @@ namespace WebApplication13.Controllers
 {
     public class HomeController : Controller
     {
-        private static List<Location> locations = new List<Location>();
-
+        private static readonly List<Location> locations = new();
         public ActionResult Index()
         {
             LoadFromExcel();
@@ -17,14 +16,13 @@ namespace WebApplication13.Controllers
         [HttpPost]
         public ActionResult AddLocation(Location newLocation)
         {
-            int maxLocationId = locations.Count > 0 ? locations.Max(l => l.LOCATION_ID) : 0;
-            newLocation.LOCATION_ID = maxLocationId + 1; 
+            newLocation.LOCATION_ID = locations.Count + 1;
             locations.Add(newLocation);
             SaveToExcel(locations);
             return RedirectToAction("Index");
         }
 
-        public ActionResult Create()
+        public IActionResult Create()
         {
             return View();
         }
@@ -44,7 +42,7 @@ namespace WebApplication13.Controllers
             Location ? locationToEdit = locations.FirstOrDefault(l => l.LocationGuid == locationGuid);
             if (locationToEdit == null)
             {
-                return View("NotFound"); 
+                return View("NotFound");
             }
 
             return View(locationToEdit);
@@ -56,20 +54,20 @@ namespace WebApplication13.Controllers
             Location ? locationToEdit = locations.FirstOrDefault(l => l.LocationGuid == locationGuid);
             if (locationToEdit == null)
             {
-                return View("NotFound"); 
+                return View("NotFound");
             }
 
             locationToEdit.LOCATION_NAME = editedLocation.LOCATION_NAME;
-            locationToEdit.LOCATION_ID = editedLocation.LOCATION_ID; 
+            locationToEdit.LOCATION_ID = editedLocation.LOCATION_ID;
             locationToEdit.IS_CLEARANCE = editedLocation.IS_CLEARANCE;
 
             SaveToExcel(locations);
             return RedirectToAction("Index");
         }
 
-        public ActionResult Delete(string locationName)
+        public ActionResult Delete(string locationGuid)
         {
-            Location? locationToDelete = locations.FirstOrDefault(l => l.LOCATION_NAME == locationName);
+            Location ? locationToDelete = locations.FirstOrDefault(l => l.LocationGuid == locationGuid);
             if (locationToDelete == null)
             {
                 return View("NotFound");
@@ -80,80 +78,53 @@ namespace WebApplication13.Controllers
             return RedirectToAction("Index");
         }
 
-        private void LoadFromExcel()
+        private static void LoadFromExcel()
         {
             if (locations.Count > 0)
             {
                 return;
             }
 
-            string filePath = @"C:\Users\harpu\Downloads\FLOOR_LOCATION3.xlsx";
-            using (var package = new ExcelPackage(new FileInfo(filePath)))
+            string filePath = @"C:\Users\harpu\Downloads\FLOOR_LOCATION1.xlsx";
+            using var package = new ExcelPackage(new FileInfo(filePath));
+            ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+            int rowCount = worksheet.Dimension.Rows;
+
+            for (int row = 2; row <= rowCount; row++)
             {
-                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
-                int rowCount = worksheet.Dimension.Rows;
-
-                for (int row = 2; row <= rowCount; row++)
+                Location location = new()
                 {
-                    Location location = new Location
-                    {
-                        LOCATION_NAME = worksheet.Cells[row, 1].Value?.ToString(),
-                    };
+                    LOCATION_NAME = worksheet.Cells[row, 1].Value?.ToString(),
+                    LOCATION_ID = int.TryParse(worksheet.Cells[row, 2].Value?.ToString(), out int locationId) ? locationId : 0,
+                    IS_CLEARANCE = string.Equals(worksheet.Cells[row, 3].Value?.ToString(), "Y", StringComparison.OrdinalIgnoreCase),
+                    LocationGuid = Guid.NewGuid().ToString()
+                };
 
-                    if (int.TryParse(worksheet.Cells[row, 2].Value?.ToString(), out int locationId))
-                    {
-                        location.LOCATION_ID = locationId;
-                    }
-                    else
-                    {
-                        location.LOCATION_ID = 0;
-                    }
-
-                    string ? isClearanceStr = worksheet.Cells[row, 3].Value?.ToString();
-                    bool isClearance;
-
-                    if (string.Equals(isClearanceStr, "Y", StringComparison.OrdinalIgnoreCase))
-                    {
-                        isClearance = true;
-                    }
-                    else if (string.Equals(isClearanceStr, "N", StringComparison.OrdinalIgnoreCase))
-                    {
-                        isClearance = false;
-                    }
-                    else
-                    {
-                        isClearance = false;
-                    }
-
-                    location.IS_CLEARANCE = isClearance;
-                    locations.Add(location);
-                }
+                locations.Add(location);
             }
         }
 
-        private void SaveToExcel(List<Location> locationsToSave)
+        private static void SaveToExcel(List<Location> locationsToSave)
         {
-            string filePath = @"C:\Users\harpu\Downloads\FLOOR_LOCATION3.xlsx";
+            string filePath = @"C:\Users\harpu\Downloads\FLOOR_LOCATION1.xlsx";
 
-            using (var package = new ExcelPackage(new FileInfo(filePath)))
+            using var package = new ExcelPackage(new FileInfo(filePath));
+            ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+
+            worksheet.Cells.Clear();
+
+            worksheet.Cells[1, 1].Value = "LOCATION_NAME";
+            worksheet.Cells[1, 2].Value = "LOCATION_ID";
+            worksheet.Cells[1, 3].Value = "IS_CLEARANCE";
+
+            for (int i = 0; i < locationsToSave.Count; i++)
             {
-                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
-
-                worksheet.Cells.Clear();
-
-                worksheet.Cells[1, 1].Value = "LOCATION_NAME";
-                worksheet.Cells[1, 2].Value = "LOCATION_ID";
-                worksheet.Cells[1, 3].Value = "IS_CLEARANCE";
-
-                for (int i = 0; i < locationsToSave.Count; i++)
-                {
-                    worksheet.Cells[i + 2, 1].Value = locationsToSave[i].LOCATION_NAME;
-                    worksheet.Cells[i + 2, 2].Value = locationsToSave[i].LOCATION_ID;
-                    worksheet.Cells[i + 2, 3].Value = locationsToSave[i].IS_CLEARANCE ? "Y" : "N";
-                }
-
-                package.Save();
+                worksheet.Cells[i + 2, 1].Value = locationsToSave[i].LOCATION_NAME;
+                worksheet.Cells[i + 2, 2].Value = locationsToSave[i].LOCATION_ID;
+                worksheet.Cells[i + 2, 3].Value = locationsToSave[i].IS_CLEARANCE ? "Y" : "N";
             }
+
+            package.Save();
         }
     }
 }
--- WebApplication13/Models/Location.cs
+++ WebApplication13/Models/Location.cs
@@ -2,7 +2,7 @@
 {
     public class Location
     {
-        public string ? LocationGuid { get; set; }
+        public String ? LocationGuid { get; set; }
         public string ? LOCATION_NAME { get; set; }
         public int LOCATION_ID { get; set; }
         public bool IS_CLEARANCE { get; set; }
